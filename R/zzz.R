3389b1d22e34ff7ca608c0ac5d209797970bb895
aminmarani <aminhosseiny@gmail.com>
2023-05-16 11:22:57 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
   pd <- utils::packageDescription(pkgname);
-  packageStartupMessage(pkgname, " v", pd$Version, " successfully loaded. See ?", pkgname, " for help. \n Papers, resources, and other materials at structuraltopicmodel.com");
+  packageStartupMessage(pkgname, " v", pd$Version, " successfully loaded the modified version. See ?", pkgname, " for help. \n Papers, resources, and other materials at structuraltopicmodel.com");
 }
